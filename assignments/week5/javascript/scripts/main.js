78e2168ceed14dfbc0693913849e858cf8343de2
rodrigo <rgomez0387@gmail.com>
2020-07-17 16:01:02 -0700

    Saved Java Script files

--- assignments/week5/javascript/scripts/main.js
+++ assignments/week5/javascript/scripts/main.js
@@ -4,14 +4,14 @@ var number1 = 10;
 var number2 = 15;
 
 //syntax
+// if (conditional) { group of code to run if is true}
 
 
 
-
-//Logical Operators
+//Conditional Operators
 // == Compares two values, becomes the boolean true if it is the same, and false if they are not the same.
 
-if (number1 == number2) {
+if (number1 == number1) {
     console.log("This if statement was true so I ran this code");
 }
 
