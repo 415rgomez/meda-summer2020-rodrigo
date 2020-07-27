af80b199558c9a486242b325bc67cd7a37df194f
rodrigo <rgomez0387@gmail.com>
2020-07-27 12:41:06 -0700

    made java objects file

---
+++ assignments/week6/javascript/scripts/main(objects).js
@@ -0,0 +1,15 @@
+//key-value pair
+// example nameOfPerson:Rodrigo
+var myArray = [
+
+    "rodrigo", "Gomez", 32, "Pizza"
+];
+var myObject = {
+    "firstName": "Rodrigo",
+    "lastName": "Gomez",
+    "age": 32,
+    "favFood": "Pizza"
+
+};
+
+console.log(myObject["firstName"] + "'s age is" + myObject["age"]);
